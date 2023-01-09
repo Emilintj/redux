45e5ca13e681bf922e34c102b5ad3b6a6436d3fa
Emilin Treesa Joseph <emilintresia02@gmail.com>
2023-01-09 22:01:24 +0530

    Fourth Commit

---
+++ products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'Maybe in another life',
+        price: 165.76,
+        image:require('./assets/image1.jpeg')
+
+    },
+    {
+        id: 2,
+        name: 'Paulo Coelho',
+        price: 123.31,
+        image:require('./assets/image2.jpeg')
+    },
+    {
+        id: 3,
+        name: 'The Politics',
+        price: 654.14,
+        image:require('./assets/image3.jpeg')
+    },
+    {
+        id: 4,
+        name: 'The Choice',
+        price: 23.90,
+        image:require('./assets/image4.jpeg')
+ 
+    },
+    {
+        id: 5,
+        name: 'The subtle art of not giving a fuck',
+        price: 100.03,
+        image:require('./assets/image5.jpeg')
+    },
+
+]
